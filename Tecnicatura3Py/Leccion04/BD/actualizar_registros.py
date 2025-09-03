33c52fdc5780f699f115fb317259285e7f966f6b
fenixnodavid <yamilrueda88@gmail.com>
2025-09-03 20:06:11 -0300

    Commit de semestre 3

---
+++ Tecnicatura3Py/Leccion04/BD/actualizar_registros.py
@@ -0,0 +1,16 @@
+import psycopg2 # Esto es para poder conectarnos a Postgres
+
+conexion=psycopg2.connect(user='postgres',password='33579857',host='127.0.0.1', port='5432',database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            valores = ('Juan Carlos', 'Roldan', 'rcarlos@mail.com', 1) # Es una tupla de tuplas
+            cursor.execute(sentencia, valores) # De esta manera ejecutamos la sentencia
+            registros_actualizados = cursor.rowcount
+            print(f'Los registros actualizados son: {registros_actualizados}')
+
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
